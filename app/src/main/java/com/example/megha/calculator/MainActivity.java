92e08dfb830d0080b914f619547d8876fb20ebb9
meghanavgurukul <meghaa18@navgurukul.org>
2018-08-31 01:22:48 +0530

    first commit

---
+++ app/src/main/java/com/example/megha/calculator/MainActivity.java
@@ -0,0 +1,25 @@
+package com.example.megha.calculator;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.CheckBox;
+
+import java.text.DecimalFormat;
+import java.util.Scanner;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+
+    @Override
+    public void onClick(View v) {
+
+
+    }
+}
